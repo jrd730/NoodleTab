38e05124f202c33c6f6c4587cc3157cabf1007e1
Jason D <jrd730@gmail.com>
2019-04-02 17:56:21 -0700

    adding annotation example

---
+++ examples/chords2.js
@@ -0,0 +1,10 @@
+var NoodleTab = require("../src/noodle-tab.js");
+
+var phrases = {
+  I: "5: 3 4: 2 3: 0 2: 1 3: 0 4: 2",
+  VI: "5: 0 4: 2 3: 2 2: 1 3: 0 4: 2",
+  start: "[ {Cmaj} @I | {Amin} @VI ]"
+};
+
+var output = NoodleTab.makeTab('start', phrases);
+console.log(output);
